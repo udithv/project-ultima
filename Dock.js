1bca1d794bb978588d1bf6a7c6189925782e8fc6
udithv <udithv01@gmail.com>
2018-04-14 22:46:11 +0530

    Added Templating

--- Dock.js
+++ Dock.js
@@ -1,4 +1,5 @@
 const fse = require('fs-extra');
+const Mustache = require('mustache');
 
 class Dock {
     constructor(uid, mid){
@@ -6,8 +7,9 @@ class Dock {
         this.mid = mid;
         this.prefix = 'schdock-';
         this.udir = `/tmp/${this.prefix}${this.uid}-${this.mid}`;
-        this.mdir = `/tmp/${this.prefix}${this.uid}-${this.mid}/models`;
-        this.fpathlist = [];
+        this.mdir = `/tmp/${this.prefix}${this.uid}-${this.mid}/models/`;
+        this.files = [];
+
     }
 
     get userDir(){
@@ -22,43 +24,101 @@ class Dock {
         return this.fpathlist;
     }
 
-    cleanUpUserDir(){
-        fse.remove(this.udir)
-            .then(() => {
-                console.log(`CleanUp Complete for ${this.udir}`);
-            })
-            .catch(err => {
-                console.error(err)
-            });
+    /*
+     * Generates the source code from view using mustache template install 
+     * Input : { path: string, jsview: Object }
+     * Output: { path: string, content: string }
+     */
+
+    genModelData(mobj){
+        return fse.readFile('./template/Model.mustache')
+                .then(tdata => tdata.toString())
+                .then(tdata =>  {
+                    let obj = {};
+                    obj['path'] = mobj.path;
+                    obj['content'] = Mustache.render(tdata,mobj.jsview)
+                    return obj;
+                });
     }
+
+    /* 
+     * Deletes the user directory used for cleaning up afterwards.
+     * 
+     */
+    cleanUpUserDir(){
+       return fse.remove(this.udir)
+                    .then(() => {
+                        return {
+                            success:true,
+                            userDir:this.udir
+                        }
+                    })
+                    .catch(err => {
+                        console.error(err)
+                        return { 
+                            success: false
+                        }
+                    });
+    }
+
+    /* 
+     *Creates a folder with name prefix-userid-modelid
+     */
 
     createTmpModelsFolder(){
-
-        fse.ensureDir(this.mdir)
-          .then(() => {
-              console.log('Model folder for user created');
-              return;
-          })
-          .catch(err => {
-              console.error(err)
-          });
-        
-        return modelDir;
+       return fse.ensureDir(this.mdir)
+                    .then(() => {
+                        console.log('Model folder for user created', this.mdir);
+                        return {
+                            success: true,
+                            modelDir: this.mdir
+                        };
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        return {
+                            success: false
+                        };
+                    });
         
     }
 
-    populateModelsDir(flist){
-        const fpathlist = flist.map(fname => {
-            return `${this.mdir}/${fname}`;
+    /* 
+     * Geneartes Files view array from the template
+     */
+
+    genFileViews(models){
+        return models.map(model => {
+            let mobj = {};
+            mobj['path'] = `${this.mdir}${model.cname}.js`;
+            mobj['filename'] = `${model.cname}.js`;
+            mobj['jsview'] = model;
+            return mobj;
         });
 
-        this.fpathlist = fpathlist;
-    
-        return Promise.all(fpathlist.map(fpath => {
-            return fse.outputFile(fpath)
-        }));
-        
     }
+
+    /* 
+     *Returns a promise which resolves an array of 
+     */
+    getFileMetadata(models){
+        return Promise.all(
+            this.genFileViews(models).map(f => this.genModelData(f))
+        );
+    }
+
+    /* 
+     *Returns a promise that resolves to boolean if successful the modelsDir
+     */
+    genModelFiles(models){
+        return this.getFileMetadata(models)
+                .then(files => {
+                    this.files = files;
+                    return this.createTmpModelsFolder()
+                });
+    }
+
+    
 }
 
 
@@ -66,5 +126,22 @@ const Dock1 = new Dock('hello','world');
 
 let fileList = ['User.js', 'Model.js','dock.js'];
 
+let models = [
+    {
+        cname: "User",
+        schemaname: function(){
+            return this.cname+'Schema';
+        }
+    },
+    {
+        cname: "Blog",
+        schemaname: function(){
+            return this.cname+'Schema';
+        }
+    }
+];
+
+Dock1.genModelFiles(models)
+      .then(x => console.log(x));
 
     
